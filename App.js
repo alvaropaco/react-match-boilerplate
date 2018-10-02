3774e0e51888216cffd48f6116fd42dbf21bfda5
Ronaldo Alves <juninho_custodio@hotmail.com>
2018-10-01 21:03:37 -0300

    3 teste

--- App.js
+++ App.js
@@ -5,11 +5,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+		<Text>Raquel s2</Text>
         <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum hendrerit mollis nunc vitae dignissim.</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-		<Text>New Line Added.</Text>
-		<Text>New modifications.</Text>
       </View>
     );
   }
