5ed1b49527dc5570b9b988a32efd0dd77282df27
Ronaldo Alves <juninho_custodio@hotmail.com>
2018-10-02 17:05:11 -0300

    Cor de fundo

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ff0000',
+    backgroundColor: '#ffff00',
     alignItems: 'center',
     justifyContent: 'center',
   },
